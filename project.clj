366b7d197e4cd28d24b7293870dda9cb51c72a98
j1n3l0 <nelo.onyiah@gmail.com>
2011-04-07 16:09:19 +0100

    Update the project.clj to point at the newly uploaded java dependency.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject transliterate "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject transliterate "1.0.0"
+  :description "Performs Perl 5-style transliterations on a given string"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.clojars.j1n3l0/jtr "1.1"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
