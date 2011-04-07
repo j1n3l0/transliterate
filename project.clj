27603f03baafddd0c4b4af652fb4153116bba470
j1n3l0 <nelo.onyiah@gmail.com>
2011-04-07 16:26:16 +0100

    Add the groupId to the project name.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject transliterate "1.0.0"
+(defproject org.clojars.j1n3l0/transliterate "1.0.0-SNAPSHOT"
   :description "Performs Perl 5-style transliterations on a given string"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
