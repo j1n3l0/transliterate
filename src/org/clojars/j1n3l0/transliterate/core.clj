7bf779a0b4720024d20c4ee2f0db2d65d6dfbb33
j1n3l0 <nelo.onyiah@gmail.com>
2011-04-08 10:00:29 +0100

    Use the correct directory structure given the namespace we're using.

---
+++ src/org/clojars/j1n3l0/transliterate/core.clj
@@ -0,0 +1,12 @@
+(ns org.clojars.j1n3l0.transliterate.core
+  (:import [org.jtr.transliterate CharacterParser CharacterReplacer]))
+
+(defn tr
+  "Performs Perl 5-style transliterations on a given string"
+  [search-list replacement-list string & options]
+  (let [flags              {:c CharacterParser/COMPLEMENT_MASK
+                            :d CharacterParser/DELETE_UNREPLACEABLES_MASK
+                            :s CharacterParser/SQUASH_DUPLICATES_MASK}
+        opt-flags          (reduce + (map #(get flags % 0) options))
+        character-replacer (doto (new CharacterReplacer search-list replacement-list) (.setFlags opt-flags))]
+    (.doReplacement character-replacer string)))
